b22a09dfbf371f5c545c37fbe43d21877c3b2b25
luis <luismigueltamsi18@gmail.com>
2026-08-15 17:24:38 +0800

    adding a simple add, delete, and input to choose

--- patient.py
+++ patient.py
@@ -1,41 +1,42 @@
 patient_names = []
 
 def add_patient(patient_name):
-    patient_name = add_patient(input("Add patient name: "))
+    patient_name = input("Add patient name: ")
     patient_names.append(patient_name)
-    for x in patient_names:
-        print(x)
+    print("Patient added successfully")
 
 def remove_patient(patient_number):
     patient_names.pop()
 
-# def edit_patient(patient_number):
+def edit_patient(patient_number):
+    patient_names[patient_number] = input("Enter new patient name: ")
+    print("Patient updated successfully")
 
-
-# def show_patient():
-
+def show_patient():
+    if len(patient_names) == 0:
+        print("No patients found")
+    else:
+        for x in patient_names:
+            print(x)
 
 def start_program():
-    isRunning = True:
-        print("1. Show patient")
-        print("2. Show patient")
-        print("3. Show patient")
+    isRunning = True
+    while isRunning:
+        print("1. Add patient")
+        print("2. remove patient")
+        print("3. edit patient")
         print("4. Show patient")
         choice = int(input("Enter number: "))
 
         if choice < 0 or choice > 4:
             print("invalid choice")
-            
-        
+        elif choice == 1:
+            add_patient(choice)
+        elif choice == 2:
+            remove_patient(choice)
+        elif choice == 3:
+            edit_patient(choice)
+        elif choice == 4:
+            show_patient()
 
-
-
-    
-    
-
-
-
-
-
-
-
+start_program()
