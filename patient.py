f8bbdaa7a79a35a2044cab72fe0796eacfc2dbc3
luis <luismigueltamsi18@gmail.com>
2026-08-18 01:35:06 +0800

    fix bugs in remove and edit function, added a condition when there is no patient it should not process further. Add some UI fix adding seperators for better visibility

--- patient.py
+++ patient.py
@@ -1,27 +1,33 @@
 patient_names = [] 
 
-def add_patient(patient_name):
+def add_patient():
     patient_name = input("Add patient name: ") 
     patient_names.append(patient_name) 
-    print("Patient added successfully") 
+    print(f"Patient {patient_name} added successfully") 
 
-def remove_patient(patient_number): 
+def remove_patient(): 
+    if len(patient_names) == 0:
+        print("No patients to remove")
+        return
     show_patient()
     patient_number = int(input("Enter patient number to remove: ")) 
 
     if patient_number > 0 and patient_number <= len(patient_names): 
         patient_names.pop(patient_number - 1) 
-        print("Patient removed successfully") 
+        print(f"Patient removed successfully") 
 
-def edit_patient(patient_number):
-    show_patient() 
+def edit_patient():
+    if len(patient_names) == 0:
+        print("No patients to edit")
+        return
     
+    show_patient() 
     patient_number = int(input("Enter patient number to edit: ")) 
 
     if patient_number > 0 and patient_number <= len(patient_names): 
         new_name = input("Enter new name: ") 
         patient_names[patient_number - 1] = new_name 
-        print("Patient name updated successfully") 
+        print(f"Patient updated successfully") 
     else:
         print("Invalid patient number") 
 
@@ -29,33 +35,45 @@ def show_patient():
     if len(patient_names) == 0: 
         print("No patients found")
     else: 
+        print("====================")
+        print("   Patient List:")
         for x, name in enumerate(patient_names):
             print(f"{x+1}: {name}")
+        print("====================")
 
-def show_menu():
+def patient_menu():
    
-    print("1. Add patient")
-    print("2. remove patient")
-    print("3. edit patient")
-    print("4. Show patient")
+    print("")
+    print("====================")
+    print(" Patient Management")
+    print("====================")
+    print("1. Show patient")
+    print("2. Add patient")
+    print("3. Edit patient")
+    print("4. Remove patient")
     print("5. Exit")
+    
+    try:
 
-    choice = int(input("Enter number: "))
+        choice = int(input("Enter number: "))
 
-    match choice:
-        case 1:
-            add_patient(choice)
-        case 2:
-            remove_patient(choice)
-        case 3:
-            edit_patient(choice)
-        case 4:
-            show_patient()
-        case 5:
-            print("Exiting...")
-            exit()
-        case _:
-            print("Invalid choice")
+        match choice:
+            case 1:
+                show_patient()
+            case 2:
+                add_patient()
+            case 3:
+                edit_patient()
+            case 4:
+                remove_patient()
+            case 5:
+                print("Exiting...")
+                exit()
+            case _:
+                print("Invalid choice")
+    except ValueError:
+        print("Invalid choice")
+
 
 if __name__ == "__main__":
     show_menu()
